222c59c42778a75022fb3faf03b6c745c55b950e
keyahR24 <rhodahkeyah@gmail.com>
2024-10-31 09:16:55 +0300

    user_input.py

---
+++ user_input.py
@@ -0,0 +1,9 @@
+# user_input.py
+
+# Collect user information
+name = "Rhodah"  
+age = "22"       
+location = "Nairobi"
+
+# Display personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
